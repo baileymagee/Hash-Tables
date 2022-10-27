ea12b88823b73b2c70ee7310ca1cd990788c7605
rajahwu <vmradford@gmail.com>
2022-10-27 17:05:15 -0400

    complete d4 long-practice phase 3

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -37,18 +37,18 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
     let currentPair = this.data[index];
 
-    while(currentPair && currentPair.key !== key) {
+    while (currentPair && currentPair.key !== key) {
       currentPair = currentPair.next;
     }
 
-    if(currentPair) {
+    if (currentPair) {
       currentPair.value = value;
       return this;
     }
 
     const newPair = new KeyValuePair(key, value);
 
-    if(this.data[index]) newPair.next = this.data[index];
+    if (this.data[index]) newPair.next = this.data[index];
     this.data[index] = newPair;
     this.count++;
 
@@ -57,7 +57,26 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   read(key) {
-    // Your code here
+
+    // for(let i = 0; i < this.data.length; i++) {
+    //   let ele = this.data[i];
+    //   if(ele === null) break;
+    //   if(ele.key === key) return ele.value;
+    // }
+    let index = this.hashMod(key)
+
+    let current = this.data[index]
+    if (current.key === key) {
+      return current.value
+    } else {
+      while (current) {
+        if (current.key === key) {
+          return current.value
+        }
+        current = current.next
+      }
+    }
+
   }
 
 
